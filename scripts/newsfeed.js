c90857b361f266d251e9f08e9c283db60b6f0ad8
Melissa Edge <melissa@edges.org>
2013-12-07 20:18:28 -0700

    Reformatted for mobile, added dialogs in newsfeed

--- scripts/newsfeed.js
+++ scripts/newsfeed.js
@@ -1,22 +1,67 @@
 $(document).ready(function(){
+	$("#signButton").button({
+		label: "Log out"
+	});
+	$("#addFriendButton").button({
+		label: "Add friend"
+	});
+	$("#boredButton").button({
+		label: "I'm bored!"
+	});
+	$("#notificationsButton").button({
+		label: "Notifications"
+	});
 	$("#addFriendButton").click( function() {
-		alert("hi");
+		$("#dialog-friend").dialog( "open" );
 	});
 	
 	$( "#boredButton" ).click(function() {
-      $( "#dialog-confirm" ).dialog( "open" );
+      $( "#dialog-activity" ).dialog( "open" );
+    });
+    
+    $( "#notificationsButton" ).click(function() {
+      $( "#dialog-notifications" ).dialog( "open" );
     });
 	
-	$( "#dialog-confirm" ).dialog({
+	$( "#dialog-activity" ).dialog({
 	  autoOpen: false,
       resizable: false,
-	  width: 400,
+	  width: 300,
+	  height: 200,
       modal: true,
       buttons: {
         "Ok": function() {
           $( this ).dialog( "close" );
         },
-        Cancel: function() {
+        "Cancel": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+    
+    
+    $( "#dialog-friend" ).dialog({
+	  autoOpen: false,
+      resizable: false,
+	  width: 300,
+	  height: 200,
+      modal: true,
+      buttons: {
+        "Ok": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });    
+    
+    
+    $( "#dialog-notifications" ).dialog({
+	  autoOpen: false,
+      resizable: false,
+	  width: 300,
+	  height: 300,
+      modal: true,
+      buttons: {
+        "Ok": function() {
           $( this ).dialog( "close" );
         }
       }
---
+++ scripts/signup.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){	
+	$("#signButton").button({
+		label: "Sign up"
+	});
+	$("#createAccount").button({
+		label: "Create Account"
+	});
+});
