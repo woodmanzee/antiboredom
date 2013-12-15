ebd65114c947807fe1bb6a8d7eff857a33a68451
mykecalderon <mykecalderon@aol.com>
2013-12-14 22:31:32 -0700

    updated i'm bored interacitons
    changed the dialog box for 'I'm bored' on the newsfeed page a bit to
    have more input boxes for the user. Also started some preliminary
    actions for when the user submits the same form. The newsfeed now
    updates with a new activity. We will still need to send the info in the
    form to the server and let the server add the new activity and send json
    back to the client to populate the new activity item on success. made
    some minor changes to the css to make some things look better or
    function properly with the changes. Also added normalize.css to help
    with cross browser consistency.

--- scripts/newsfeed.js
+++ scripts/newsfeed.js
@@ -23,14 +23,55 @@ $(document).ready(function(){
       $( "#dialog-notifications" ).dialog( "open" );
     });
 	
+	function create_activity() {
+	  // get the values from the form
+	  var what = $('#dialog-activity #what_field input').val();
+	  var where = $('#dialog-activity #where_field input').val();
+	  var when = $('#dialog-activity #when_field input').val();
+	  var details = $('#dialog-activity #details_field textarea').val();
+	  
+	  // debugging, look at the values
+	  console.log(what);
+	  console.log(where);
+	  console.log(when)
+	  console.log(details)
+	  
+	  // create a newsfeed item with the values
+	  var newsItem = $('#news_item_placeholder').clone();
+	  // TODO: populate the new values with the json returned from
+	  // the server after a successful add
+	  newsItem.attr('id', 'newID');
+	  newsItem.find('img.news_user_icon').attr('id', 'newID');
+	  newsItem.find('span.news_user_name').text('Someone');
+	  newsItem.find('span.news_activity_name').text(what);
+	  $('#boredFeedBox').prepend(newsItem);
+	  newsItem.show();
+	  
+	}
+	
+	/* Note: At present, using .val() on textarea elements strips carriage return 
+	characters from the browser-reported value. When this value is sent to the 
+	server via XHR however, carriage returns are preserved (or added by browsers 
+	which do not include them in the raw value). A workaround for this issue can 
+	be achieved using a valHook as follows:
+	http://api.jquery.com/val/
+	*/
+	$.valHooks.textarea = {
+	  get: function( elem ) {
+		return elem.value.replace( /\r?\n/g, "\r\n" );
+	  }
+	};
+	
 	$( "#dialog-activity" ).dialog({
 	  autoOpen: false,
       resizable: false,
-	  width: 300,
-	  height: 200,
+	  width: 350,
+	  height: 550,
+	  title: "Create Activity",
       modal: true,
       buttons: {
-        "Ok": function() {
+        "Create": function() {
+		  create_activity();
           $( this ).dialog( "close" );
         },
         "Cancel": function() {
@@ -47,7 +88,7 @@ $(document).ready(function(){
 	  height: 200,
       modal: true,
       buttons: {
-        "Ok": function() {
+        "Add Friend": function() {
           $( this ).dialog( "close" );
         }
       }
