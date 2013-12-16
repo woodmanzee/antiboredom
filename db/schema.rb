cdd20abe130108a2833b7bd70f3a034016cbc5a0
Zack Woodmansee <zack@touchpointcrm.com>
2013-12-15 20:40:01 -0700

    changed databse from sqllite3 to postgres for compatibility with heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131207023439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: true do |t|
     t.string   "userid"
     t.string   "activityid"
@@ -64,7 +67,7 @@ ActiveRecord::Schema.define(version: 20131207023439) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
