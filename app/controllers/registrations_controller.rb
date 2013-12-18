8424a95b91354a1089cebfd58b49ad2c87a8ac6c
Zack Woodmansee <zack@touchpointcrm.com>
2013-12-18 15:52:39 -0700

    should be able to login/out

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,35 @@
+class RegistrationsController < Devise::RegistrationsController
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
+  prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy, :show]
+
+  # GET /resource/sign_up
+  def new
+    super
+  end
+
+  # POST /resource
+  def create
+    super
+  end
+
+  # GET /resource/edit
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
+  # DELETE /resource
+  def destroy
+    super
+  end
+
+  def show
+  end
+
+
+  protected
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Antiboredom::Application.routes.draw do
-  devise_for :users
-
   resources :invites
   resources :activities
   resources :friends
+
+  devise_for :users
 
   get "pages/home"
   get "pages/newsfeed"
   get "pages/signup"
 
-  root :to => "sign_in#new"
-
-  devise_scope :user do
-    get "sign_in", :to => "devise/sessions#new"
+  authenticated :user do
+    root :to => "pages#newsfeed", as: :authenticated_root
   end
+
+  devise_scope :user do 
+    get 'users/sign_out' => 'devise/sessions#destroy',
+      :path_prefix => 'd'
+    get 'users/sign_in' => 'devise/sesions#new'
+
+  root :to => "devise/sessions#new"
+  end
+
 end
