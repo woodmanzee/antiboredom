a9e0c9502e3e6713a3c8fb18e460dc6d4b415d06
Zack Woodmansee <zack@touchpointcrm.com>
2013-12-18 13:12:37 -0700

    added config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Antiboredom::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = ['*.js', '*.css']
+  config.active_support.deprecation = :silence
 
   # Generate digests for assets URLs.
   config.assets.digest = true
