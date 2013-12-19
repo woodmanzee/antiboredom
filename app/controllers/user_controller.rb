e29c68bbca37503704a676485bcf0ea7ae602c85
Melissa Edge <melissa@edges.org>
2013-12-19 00:55:31 -0700

    use "/user/cur" to get info on the current user, now displaying usernames

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,10 @@ class UserController < ApplicationController
 	# GET /user/1.json
 	def show
 		id = params[:id]
+		if params[:id].to_s == "cur"
+			id = current_user.id
+		end
 		@user = User.find(id)
 	end
+
 end
