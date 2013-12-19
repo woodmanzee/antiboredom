11859a4bcee3a7b4ec045f2555c47e63a7a7e93e
Zack Woodmansee <zack@touchpointcrm.com>
2013-12-19 16:30:43 -0700

    username field can now be set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,23 @@ class RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
+    #super
+    build_resource(registration_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigation_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#(resource.inactive_message)" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
   end
 
   # GET /resource/edit
@@ -31,5 +47,11 @@ class RegistrationsController < Devise::RegistrationsController
 
 
   protected
+
+  private
+
+  def registration_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
 
 end
